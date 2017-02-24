95596774be68084c56eb516c13ef18f32fc86540
Pablo Cid <pabloagronomo@gmail.com>
2017-02-24 12:35:31 -0300

    f4

--- controllers/index.js
+++ controllers/index.js
@@ -53,12 +53,18 @@ var fs =
         });
 
         router.get('/tomafoto', function(req, res) {
-            var args = ["--timeout", "1", "-o", "-"];
+            var args = ["--nopreview", "--timeout", "1", "-o", "-"];
 
-            if (req.query.width) {
-                args.push('-w');
-                args.push(req.query.width);
+            var keys = Object.keys(req.query);
+
+            if (keys.length) {
+                for (var i = 0; i < keys.length; i++) {
+                    var el = keys[i];
+                    args.push('--' + el);
+                    args.push(req.query[el]);
+                }
             }
+
             var still = spawn('raspistill', args);
 
             still.stdout.pipe(res);
